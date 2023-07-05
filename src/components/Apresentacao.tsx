14bc24e49240830e97ff36c433e31b86da09f34e
RennanAtes <RennanAugustoBR@Gmail.com>
2023-07-05 15:54:15 -0300

    Atualizações gerais.

--- src/components/Apresentacao.tsx
+++ src/components/Apresentacao.tsx
@@ -12,7 +12,7 @@ export default function Apresentacao(){
           <Image className="rounded-full" height={200} width={200} alt='minha foto' src='https://avatars.githubusercontent.com/u/108353021?v=4'></Image>
         </div>
         <div className="flex flex-col justify-center w-full items-center">
-          <h1 className="text-center text-black text-xl">Rennan Augusto</h1>
+          <h1 className="text-center text-black text-5xl">Olá, eu sou Rennan Augusto</h1>
           <div className="text-center p-5">
             <span className="text-gray-700">Olá eu sou rennan asufsuhbsdu udsufg sdufsdj mnadfn sdjnfjdsn fjiadsnf jidsfndjsnf djfn jdnfds fds afsdfsaf asdfdsfdsf safsdf dsfasdfadsf dsfsadsf sadsad</span>
           </div>
