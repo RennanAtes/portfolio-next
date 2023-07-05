4816708f92c5a8209e6178766ce042bdb0409192
RennanAtes <RennanAugustoBR@Gmail.com>
2023-07-04 22:43:48 -0300

    Componente da apresentação inicial.

---
+++ src/components/Apresentacao.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Apresentacao(){
+    return (
+        <div className={`
+        flex
+        flex-col
+        justify-center 
+        
+        `}>
+        <div className="p-10 flex justify-center">
+          <Image className="rounded-full" height={200} width={200} alt='minha foto' src='https://avatars.githubusercontent.com/u/108353021?v=4'></Image>
+        </div>
+        <div className="flex flex-col justify-center w-full items-center">
+          <h1 className="text-center text-black text-xl">Rennan Augusto</h1>
+          <div className="text-center p-5">
+            <span className="text-gray-700">Olá eu sou rennan asufsuhbsdu udsufg sdufsdj mnadfn sdjnfjdsn fjiadsnf jidsfndjsnf djfn jdnfds fds afsdfsaf asdfdsfdsf safsdf dsfasdfadsf dsfsadsf sadsad</span>
+          </div>
+        </div>      
+      </div>
+    )
+}
