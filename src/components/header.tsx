2282431fcf3e7a2121d4817ad94f47f775e46cb0
RennanAtes <RennanAugustoBR@Gmail.com>
2023-07-04 22:43:05 -0300

    Header utilizando Styled-components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,31 @@
+"use client"
+import { styled } from 'styled-components'
+
 export default function Header(){
+
+    const TagHeader = styled.header`
+        width: 100%;
+        height: 80px;
+        align-items: center;
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 1px solid gray;
+    `
+
+
+
+
+
     return (
-        <div>
-            <h2>Header</h2>
-        </div>
+        <TagHeader className='container'>
+            <div className=''>Logo</div>
+            <div>
+                <ul className='flex gap-5'>
+                    <li className='hover:text-cyan-400'>Inicio</li>
+                    <li className='hover:text-cyan-400'>Sobre mim</li>
+                    <li className='hover:text-cyan-400'>Projetos</li>
+                </ul>
+            </div>
+        </TagHeader>
     )
 }
